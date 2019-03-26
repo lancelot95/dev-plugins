d9fe9a2274f68360ee69ad8c83de278f89e76466
lancelot goelen <lancelot95>
2019-03-26 13:54:28 +0100

    first commit

---
+++ ratatouille.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Ratatouille
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Premier plugin
+ * Author:          LANCELOT
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ratatouille
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ratatouille
+ */
+// Your code starts here.
